a3d43b5e993ef715b178280a76096b814567de6c
Rajukst <rajuahamedkst15@gmail.com>
2023-11-02 11:39:08 +0600

    gyhjk

--- src/Pages/UserList/UserList.jsx
+++ src/Pages/UserList/UserList.jsx
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery } from "react-query";
 import React, { useState } from "react";
 import { Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
